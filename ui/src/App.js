c3931169c53fd11e18d1f706e65b36823444f0c9
Tony Kelly <patient-remix-0q@icloud.com>
2022-04-10 21:52:47 -0400

    feat: forms three default plots

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+
+// Third Party Components
+import Plot from 'react-plotly.js';
+import MakePlot from './components/MakePlot/MakePlot';
+
+// Example Data Files
+let cumetal = require('./referenceData/cu_metal.json');
 
 function App() {
+
+  const [energy, setEnergy] = useState([]);
+  const [i0, setI0] = useState([]);
+  const [itrans, setItrans] = useState([]);
+  const [uE, setUE] = useState([]);
+
+  useEffect( () => {
+    setEnergy(cumetal.data.map( (entry) => entry.energy ));
+    setI0(cumetal.data.map( (entry) => entry.i0 ));
+    setItrans(cumetal.data.map( (entry) => entry.itrans ));
+  },[])
+
+  useEffect( () => {
+    let val = [];
+    for (let i = 0; i < i0.length; i++) {
+      let temp = i0[i]/itrans[i];
+      val.push(Math.log(temp));
+    }
+    setUE(val);
+  }, [i0, itrans])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MakePlot xData={energy} yData={i0} title="Incident Intensity" xText="Energy (eV)" yText="i0" />
+      <MakePlot xData={energy} yData={itrans} title="Transmitted Intensity" xText="Energy (eV)" yText="itrans" />
+      <MakePlot xData={energy} yData={uE} title="Absorption Coefficient" xText="Energy (eV)" yText="uE" />
     </div>
   );
 }
---
+++ ui/src/components/MakePlot/MakePlot.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// Third Party Components
+import Plot from 'react-plotly.js';
+
+const MakePlot = ({xData, yData, title, xText, yText}) => {
+  return (
+    <Plot
+        data={[
+          {
+            x: xData,
+            y: yData,
+            type: 'scatter',
+            mode: 'lines+markers',
+            marker: {color: 'red'},
+          },
+        ]}
+        layout={ 
+          { width: 600, 
+            height: 600, 
+            title: title, 
+            xaxis: {
+              title: {
+                text: xText
+              }
+            },
+            yaxis: {
+              title: {
+                text: yText
+              }
+            }
+          } 
+        }
+      />
+  )
+}
+
+export default MakePlot;
